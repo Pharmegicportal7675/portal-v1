e797dc18c3e35d1cb0af8eae63fff107b579f374
Atul Patoliya <support@pharmegichealthcare.com>
2026-06-23 17:06:19 +0530

    code

--- next.config.js
+++ next.config.js
@@ -1,23 +1,32 @@
-/** @type {import('next').NextConfig} */
-const PDF_FONT_TRACE = ['./public/fonts/**'];
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  serverExternalPackages: ['puppeteer-core', '@sparticuz/chromium-min'],
-  experimental: {
-    optimizePackageImports: ['lucide-react', 'recharts'],
-  },
-  outputFileTracingIncludes: {
-    '/api/reach-certificate/pdf-html': [
-      './node_modules/@sparticuz/chromium-min/**',
-      ...PDF_FONT_TRACE,
-    ],
-    '/api/tcc-certificate/pdf-html': [
-      './node_modules/@sparticuz/chromium-min/**',
-      ...PDF_FONT_TRACE,
-    ],
-    '/api/tcc-certificate/html-data': PDF_FONT_TRACE,
-  },
-};
-
-module.exports = nextConfig;
+/** @type {import('next').NextConfig} */
+const PDF_FONT_TRACE = ['./public/fonts/**'];
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  serverExternalPackages: ['puppeteer-core', '@sparticuz/chromium-min'],
+  experimental: {
+    optimizePackageImports: ['lucide-react', 'recharts'],
+  },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/login',
+        permanent: false,
+      },
+    ];
+  },
+  outputFileTracingIncludes: {
+    '/api/reach-certificate/pdf-html': [
+      './node_modules/@sparticuz/chromium-min/**',
+      ...PDF_FONT_TRACE,
+    ],
+    '/api/tcc-certificate/pdf-html': [
+      './node_modules/@sparticuz/chromium-min/**',
+      ...PDF_FONT_TRACE,
+    ],
+    '/api/tcc-certificate/html-data': PDF_FONT_TRACE,
+  },
+};
+
+module.exports = nextConfig;
